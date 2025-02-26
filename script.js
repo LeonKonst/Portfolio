93b816afa293e88842f33b76de13c85b160f81d3
Leontiadis Konstantinos <113901432+LeonKonst@users.noreply.github.com>
2025-02-26 23:02:31 +0200

    splitting js files data and logic

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-// Info that will be displayed in the webpage
 
-const personalInfo = {
-    name:"Konstantinos",
-    surname:"Leontiadis",
-    location:{country:"Denmark",
-    city:"Copenhagen",},
-    email:"le.kostas93@gmail.com",
-    phonenumber:{prefix:"+45",
-        number:"11 22 33 44",
-    },
-    linkedin:{profilelink:"https://www.linkedin.com/in/konleon/",
-        name:"Konstantinos Leontiadis, Ph.D.",
-    },
-    github:{profilelink:"https://github.com/LeonKonst",
-        name:"LeonKonst",
-    },
-
-}
-
-const titles = [
-    ["Chemical Engineer", "CM"],
-    ["Polymer Scientist","PS"],
-    ["Material Scientist", "MS"],
-    ["Web developer", "WD"],
-];
 
 
 // get HTML elements to display the info above
